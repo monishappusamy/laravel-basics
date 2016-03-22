56b0d3ff0ab4ded02d1963a39d67bb1e827c591b
monish appusamy <monishappusamy@gmail.com>
2016-03-22 12:59:10 -0400

    The dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,52 @@
-<h1>The dashboard</h1>
+@extends('layouts.master')
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header>
+                <h3>what do you have to say!</h3>
+                <form action="">
+                    <div class="form-group">
+                        <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your post"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create Post</button>
+                </form>
+            </header>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say...</h3></header>
+            <article class="post">
+                <p>
+                    Lorem ipsum dolor sit amet, duo tollit scribentur te. Ne sit recusabo conclusionemque, cum an iudicabit appellantur. No per eruditi propriae instructior, quo te modo eripuit.
+                    Tation albucius delicatissimi in eum.
+                </p>
+                <div class="info">
+                    Posted by Monish on 20 Mar 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a> |
+                </div>
+            </article>
+            <article class="post">
+                <p>
+                    Lorem ipsum dolor sit amet, duo tollit scribentur te. Ne sit recusabo conclusionemque, cum an iudicabit appellantur. No per eruditi propriae instructior, quo te modo eripuit.
+                    Tation albucius delicatissimi in eum.
+                </p>
+                <div class="info">
+                    Posted by Monish on 20 Mar 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a> |
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
